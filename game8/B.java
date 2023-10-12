06c4b8ce39822c651b94c4895cd34d702f9b90bb
honmakotaro <19164@hakodate.kosen-ac.jp>
2023-10-12 09:55:37 +0900

    Bがキャラ追加

--- game8/B.java
+++ game8/B.java
@@ -6,11 +6,11 @@ import greenfoot.*;
 /**
  * 
  */
-public class A extends Actor
+public class B extends Actor
 {
 
     /**
-     * Act - do whatever the A wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
+     * Act - do whatever the B wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
