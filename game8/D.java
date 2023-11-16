4eb4b3e71e146d008ca80572248c1a09dadcd513
RyotaMiyahara1023 <nakaichi1728@gmail.com>
2023-11-16 10:15:05 +0900

    コミット

--- game8/D.java
+++ game8/D.java
@@ -8,15 +8,12 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class D extends Actor
 {
-    private int height = 50;
-    private int width = 50;
-
     /**
      * 
      */
     public D()
     {
-        getImage().scale(width, height);
+        getImage().scale(50, 70);
     }
     /**
      * Act - do whatever the D wants to do. This method is called whenever
@@ -24,6 +21,21 @@ public class D extends Actor
      */
     public void act() 
     {
-        // Add your action code here.
+        if( Greenfoot.isKeyDown( "right" ) ){
+            setRotation(0);
+            move(1);
+        }
+        if( Greenfoot.isKeyDown( "left" ) ){
+            setRotation(0);
+            move(-1);
+        }
+        if( Greenfoot.isKeyDown( "up" ) ){
+            setRotation(-90);
+            move(1);
+        }
+            if( Greenfoot.isKeyDown( "down" ) ){
+            setRotation(90);
+            move(1);
+        }
     }    
 }
